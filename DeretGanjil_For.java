eb88622fdc0a70c439707ae126cedb19accbcf43
Zulfikar <zulfikar97@gmail.com>
2015-12-09 21:58:52 +0700

    Create DeretGanjil_For.java

---
+++ DeretGanjil_For.java
@@ -0,0 +1,15 @@
+public class DeretGanjil_For {
+    public static void main(String []args){
+        int a = 1;
+        for (int i = 0; i < 10; i++){
+            System.out.print( a + " ");
+            a = a + 2;
+        }
+    }
+}
+
+/*
+Output yg dicari
+1 3 5 7 ...
+hingga suku ke 10
+*/
