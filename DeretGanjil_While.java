6becce99c6e9200e0cfbc248bdbbe26c46536940
Zulfikar <zulfikar97@gmail.com>
2015-12-09 22:02:26 +0700

    Create DeretGanjil_While.java

---
+++ DeretGanjil_While.java
@@ -0,0 +1,17 @@
+public class DeretGanjil_While {
+    public static void main(String []args){
+        int a = 1;
+        int i = 0;
+        while (i<10){
+            System.out.print(a+" ");
+            a=a+2;
+            i++;
+        }
+    }
+}
+
+/*
+Output yg dicari deret bilangan ganjil
+1 3 5 7 ...
+hingga suku ke-10 dengan perintah perulangan/looping (while)
+*/
